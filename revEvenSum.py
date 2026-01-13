366c6e8be1e3637424112a1ec19b34f13d768679
guru1121 <gurunandm@gmail.com>
2026-01-13 09:16:11 +0530

    revEvensum

---
+++ revEvenSum.py
@@ -0,0 +1,10 @@
+# write a program using loop to print first 10 even numbers in reverse order
+
+num = 20
+
+while num > 0:
+    # print(num)
+    num = num -2
+    
+for x in range(22, 1 , -2):
+    print(x)
